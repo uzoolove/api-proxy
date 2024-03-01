b5a2d8ff20af35477c36497f23e7cc0e65d6e866
Kilyong Jeong <uzoolove@gmail.com>
2024-03-01 21:57:12 +0900

    🔧

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const options = {
   ws: true, // proxy websockets
   router: {
     'motizen.frontendschool.shop:33080': 'http://frontendschool.shop:33010',
-    'todo.frontendschool.shop:443': 'http://frontendschool.shop:33010',
-    'todo-api.frontendschool.shop:443': 'http://frontendschool.shop:33020',
-    'frontendschool.shop:443': 'http://frontendschool.shop:33030',
-    'api.frontendschool.shop:443': 'http://frontendschool.shop:33040',
+    'todo.frontendschool.shop': 'http://frontendschool.shop:33010',
+    'todo-api.frontendschool.shop': 'http://frontendschool.shop:33020',
+    'frontendschool.shop': 'http://frontendschool.shop:33030',
+    'api.frontendschool.shop': 'http://frontendschool.shop:33040',
   },
 };
 
