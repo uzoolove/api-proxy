6b02f16a2d0e835670e1034e35a16e98733cb6e5
Kilyong Jeong <uzoolove@gmail.com>
2024-03-01 21:21:36 +0900

    ✨ init

---
+++ index.js
@@ -0,0 +1,54 @@
+// include dependencies
+import express from 'express';
+import cors from 'cors';
+import proxy from 'http-proxy-middleware';
+import greenlock from 'greenlock-express';
+
+
+
+
+
+// proxy middleware options
+const options = {
+  target: 'http://frontendschool.shop:33080', // target host
+  changeOrigin: true, // needed for virtual hosted sites
+  ws: true, // proxy websockets
+  router: {
+    'motizen.frontendschool.shop:33080': 'http://frontendschool.shop:33010',
+    'todo.frontendschool.shop:443': 'http://todo.frontendschool.shop:33010',
+    'todo-api.frontendschool.shop:443': 'http://todo-api.frontendschool.shop:33020',
+    'shopping.frontendschool.shop:443': 'http://shopping.frontendschool.shop:33030',
+    'shopping-api.frontendschool.shop:443': 'http://shopping-api.frontendschool.shop:33040',
+  },
+};
+
+// create the proxy (without context)
+const todoApiProxy = proxy.createProxyMiddleware(options);
+
+// mount `exampleProxy` in web server
+const app = express();
+
+app.use('/', (req, res, next) => {
+  console.log(req.headers)
+  next();
+});
+
+app.use(cors({
+  origin: [
+    /^https?:\/\/localhost/,
+    /^https?:\/\/127.0.0.1/,
+    new RegExp(process.env.DOMAIN)
+  ],
+  credentials: true,
+}));
+
+app.use('/', todoApiProxy);
+
+greenlock.init({
+  packageRoot: '.',
+  configDir: './greenlock.d',
+  maintainerEmail: "uzoolove@gmail.com",
+  cluster: false
+}).serve(app); // Serves on 80 and 443
+
+// app.listen(33080);
